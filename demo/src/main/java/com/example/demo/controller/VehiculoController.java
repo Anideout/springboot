68cf05733270d98f505cca83efe820586b2210cc
unknown <CETECOM@AOAWSB02LC0720.duoc.acad>
2025-04-23 23:20:52 -0400

    clase 23-04-25, poniendo en uso metodos abstractos

--- demo/src/main/java/com/example/demo/controller/VehiculoController.java
+++ demo/src/main/java/com/example/demo/controller/VehiculoController.java
@@ -1,20 +1,31 @@
 package com.example.demo.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.demo.model.Vehiculo;
+import com.example.demo.repository.VehiculoRepository;
 
 @RestController
 @RequestMapping("/vehiculos")
 public class VehiculoController {
+
     @Autowired
-     private VehiculoController repository;
+     private VehiculoRepository repository;
     
     @GetMapping
     public List<Vehiculo> obtenerVehiculos(){
-        return reposit
+        return repository.findAll();
+           
     }
-    
-    
+
+    @GetMapping("/persona/{rut}")
+    public List<Vehiculo> buscarPorRut(@PathVariable String rut) {
+        return repository.findByPersonaRut(rut);
+    }
 }
--- demo/src/main/java/com/example/demo/repository/VehiculoRepository.java
+++ demo/src/main/java/com/example/demo/repository/VehiculoRepository.java
@@ -1,11 +1,17 @@
 package com.example.demo.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.example.demo.model.Vehiculo;
 
-public interface VehiculoRepository 
-extends JpaRepository<Vehiculo, Long>
-{
+public interface VehiculoRepository extends JpaRepository<Vehiculo, Long> {
+    
+    List<Vehiculo> findByPersonaRut(String rut);
 
+    /*ES LO QUE SE ENTIENDE POR FINDBY 
+    SELECT * FROM VEHICULO 
+     WHERE rut =
+    */
 }
